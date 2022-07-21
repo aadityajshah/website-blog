8a05187019ed61e7d53ddc35f0b93777721740a6
Aaditya (AJ) Shah <adu.shah@gmail.com>
2022-07-21 12:54:55 -0400

    Update CSS, and Index for new site

--- pages/index.js
+++ pages/index.js
@@ -4,7 +4,17 @@ import styles from '../styles/Home.module.css'
 
 
 export default function Home() {
-  return ('Home')
-}
+  return (
+    <main className={styles.main}>
+        <h1 className={styles.title}>
+          <a href="https://www.aadityashah.com">
+            <Image src="/images/face.png" alt="my face" width='400' height='400'></Image>
+          </a>
+        </h1>
 
-
+        <p className={styles.description}>
+          {' '}
+          <code className={styles.code}>developer.foodie.gadget freak.techie.enthusiast.</code>
+        </p>
+    </main>
+  )}
